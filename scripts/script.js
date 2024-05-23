01a7ca2693704edd9b6f9a45d0a7d8c1292f7a87
silverblade34 <mpacheco.tacay@gmail.com>
2024-05-22 22:01:35 -0500

    se esta agregando proyecto de yanapay yachacuyta y edicion de descripcion sobre mi

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,28 @@
-// -------------------------Obtener los elementos de navegación de mi header-----------------------
-// Obtener los elementos de navegación de mi header
-var navItems = document.querySelectorAll('.item a');
-
-// Agregar evento de desplazamiento al documento
-document.addEventListener('scroll', function () {
-  // Obtener la posición de desplazamiento actual
-  var scrollPosition = window.scrollY;
-
-  // Inicializar la sección activa
-  var activeSection = null;
-  var activeSectionHeight = 0;
-
-  // Iterar sobre los sections para verificar la posición
-  document.querySelectorAll('main section').forEach(function (section) {
-    var sectionId = section.getAttribute('id');
-    var sectionOffsetTop = section.offsetTop;
-
-    // Verificar si el usuario está en el section actual
-    if (scrollPosition >= sectionOffsetTop && scrollPosition < sectionOffsetTop + section.offsetHeight) {
-      // Verificar si esta sección es la más grande
-      if (section.offsetHeight > activeSectionHeight) {
-        activeSection = sectionId;
-        activeSectionHeight = section.offsetHeight;
-      }
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  const hoverImageYanapay = document.getElementById('hoverImageYanapay');
+  
+  hoverImageYanapay.addEventListener('mouseenter', () => {
+    hoverImageYanapay.src = './assets/presentation_yanapay2.png';
   });
 
-  // Eliminar la clase 'active' de todos los elementos de navegación
-  navItems.forEach(function (item) {
-    item.parentElement.classList.remove('active');
+  hoverImageYanapay.addEventListener('mouseleave', () => {
+    hoverImageYanapay.src = './assets/presentation_yanapay.png';
   });
 
-  // Agregar la clase 'active' al elemento de navegación correspondiente al section actual
-  document.querySelector('.item a[href="#' + activeSection + '"]').parentElement.classList.add('active');
+  const hoverImageAduanas = document.getElementById('hoverImageAduanas');
+  
+  hoverImageAduanas.addEventListener('mouseenter', () => {
+    hoverImageAduanas.src = './assets/presentation_aduanas2.png';
+  });
+
+  hoverImageAduanas.addEventListener('mouseleave', () => {
+    hoverImageAduanas.src = './assets/presentation_aduanas.png';
+  });
 });
+
+function scrollToSection(sectionId) {
+  const targetSection = document.getElementById(sectionId);
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
